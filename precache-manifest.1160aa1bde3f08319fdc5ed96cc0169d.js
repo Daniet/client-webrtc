ef2e5e486c290cff3d87755900f10d273a6f927c
Daniel Andrés <daniel.andres.dev@gmail.com>
2020-10-31 10:47:47 -0500

    vaa

---
+++ precache-manifest.1160aa1bde3f08319fdc5ed96cc0169d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdeb4f8b5c1f1d82f4ccc26858b221ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a48cd35b1778501a300",
+    "url": "/static/css/main.b6a5705a.chunk.css"
+  },
+  {
+    "revision": "5d02cd0b9400d1fc6827",
+    "url": "/static/js/2.fdaf3765.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fdaf3765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a48cd35b1778501a300",
+    "url": "/static/js/main.2612949b.chunk.js"
+  },
+  {
+    "revision": "9048dd95493fb2095d5d",
+    "url": "/static/js/runtime-main.a69dbb19.js"
+  },
+  {
+    "revision": "cfce6abbbff0099b15691345d8b94dcc",
+    "url": "/static/media/GOTHIC.cfce6abb.TTF"
+  },
+  {
+    "revision": "f24d4b4eca0c3e59e8b7fbc7ef68f535",
+    "url": "/static/media/jesus-loves-austin-0O0gux4OZX4-unsplash.f24d4b4e.jpg"
+  },
+  {
+    "revision": "5cff935a44efbeb427cce90ff189bdbd",
+    "url": "/static/media/web.5cff935a.svg"
+  }
+]);
